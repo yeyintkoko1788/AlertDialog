e25f97f964241508e288423a420b43bddcf56a41
yeyintkoko <yeyintkoko.1788@gmail.com>
2019-07-01 13:56:32 +0630

    Update

--- app/src/main/java/com/yeyint/customdialog/MainActivity.java
+++ app/src/main/java/com/yeyint/customdialog/MainActivity.java
@@ -1,10 +1,12 @@
 package com.yeyint.customdialog;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.yeyint.customalertdialog.CustomAlertDialog;
 
@@ -13,10 +15,34 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn)
-    Button btn;
+    @BindView(R.id.btn_default)
+    Button btn_default;
 
-    private android.support.v7.app.AlertDialog alertDialog = null;
+    @BindView(R.id.btn_noactionbar)
+    Button btn_noactionbar;
+
+    @BindView(R.id.dialog_type_group)
+    RadioGroup dialog_type_group;
+
+    @BindView(R.id.et_title)
+    EditText et_title;
+
+    @BindView(R.id.et_message)
+    EditText et_message;
+
+    @BindView(R.id.et_positive)
+    EditText et_positive;
+
+    @BindView(R.id.et_negative)
+    EditText et_negative;
+
+    @BindView(R.id.btn_curve)
+    Button btn_curve;
+
+    @BindView(R.id.btn_fill)
+    Button btn_fill;
+
+    private CustomAlertDialog.DialogType type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +51,86 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn_default.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAlert();
+                showAlert(CustomAlertDialog.DialogStyle.DEFAULT);
+            }
+        });
+
+        btn_noactionbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlert(CustomAlertDialog.DialogStyle.NO_ACTION_BAR);
+            }
+        });
+
+        btn_curve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlert(CustomAlertDialog.DialogStyle.CURVE);
+            }
+        });
+
+        btn_fill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlert(CustomAlertDialog.DialogStyle.FILL_STYLE);
             }
         });
     }
 
-    private void showAlert(){
-        final CustomAlertDialog dialog = new CustomAlertDialog(this);
-        dialog.setAlertMessage("This dialog is created by Ye Yint Ko Ko. Date 26.6.2019.");
-        dialog.setPositiveButton("Ok", new View.OnClickListener() {
+    private void showAlert(CustomAlertDialog.DialogStyle style){
+        int radioButtonID = dialog_type_group.getCheckedRadioButtonId();
+        View radioButton = dialog_type_group.findViewById(radioButtonID);
+        int idx = dialog_type_group.indexOfChild(radioButton);
+        getDialogType(idx);
+        final CustomAlertDialog dialog = new CustomAlertDialog(this, style);
+        dialog.setAlertMessage(et_message.getText().toString());
+        if (style != CustomAlertDialog.DialogStyle.FILL_STYLE){
+            dialog.setAlertTitle(et_title.getText().toString());
+        }
+        dialog.setPositiveButton(et_positive.getText().toString(), new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn.setText("Testing");
+                Toast.makeText(getApplicationContext(),et_positive.getText().toString(),Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             }
         });
-        dialog.setNegativeButton("Close", new View.OnClickListener() {
+        dialog.setNegativeButton(et_negative.getText().toString(), new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn.setText("Press");
+                Toast.makeText(getApplicationContext(),et_negative.getText().toString(),Toast.LENGTH_SHORT).show();
                 dialog.cancel();
             }
         });
+        dialog.setDialogType(type);
+        if (style == CustomAlertDialog.DialogStyle.NO_ACTION_BAR){
+            dialog.setDialogImage(getDrawable(R.drawable.ic_warning_black_24dp));
+        }
         dialog.create();
         dialog.show();
+    }
+
+    private void getDialogType(int index){
+        switch (index){
+            case 0:
+                type = CustomAlertDialog.DialogType.DEFAULT;
+                break;
+            case 1:
+                type = CustomAlertDialog.DialogType.ERROR;
+                break;
+            case 2:
+                type = CustomAlertDialog.DialogType.SUCCESS;
+                break;
+            case 3:
+                type = CustomAlertDialog.DialogType.INFO;
+                break;
+            case 4:
+                type = CustomAlertDialog.DialogType.WARNING;
+                break;
+                default:
+                    type = CustomAlertDialog.DialogType.DEFAULT;
+        }
     }
 }
--- customalertdialog/src/main/java/com/yeyint/customalertdialog/CustomAlertDialog.java
+++ customalertdialog/src/main/java/com/yeyint/customalertdialog/CustomAlertDialog.java
@@ -1,26 +1,75 @@
 package com.yeyint.customalertdialog;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class CustomAlertDialog extends AlertDialog {
 
     private Context context;
     private View view;
-    private AlertDialog alertDialog = null;
+    public enum DialogType {
+        DEFAULT,
+        ERROR,
+        SUCCESS,
+        INFO,
+        WARNING
+    }
 
-    public CustomAlertDialog(@NonNull Context context) {
+    public enum DialogStyle{
+        DEFAULT,
+        NO_ACTION_BAR,
+        CURVE,
+        FILL_STYLE
+    }
+
+    private Button positiveButton;
+    private Button negativeButton;
+    private TextView positiveText;
+    private TextView negativeText;
+    private TextView alertMessage;
+    private TextView alertTitle;
+    private LinearLayout llTitleLayout;
+    private ImageView dialogImage;
+    private LinearLayout mainLayout;
+
+    private DialogStyle style = DialogStyle.DEFAULT;
+
+    public CustomAlertDialog(@NonNull Context context, DialogStyle dialogStyle) {
         super(context,R.style.MyDialogTheme);
         this.context = context;
-        LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view = li.inflate(R.layout.default_alert, null);
-        initialize(context, view);
+        switch (dialogStyle){
+            case DEFAULT:
+                initialize(context);
+                style = DialogStyle.DEFAULT;
+                break;
+            case NO_ACTION_BAR:
+                initializeNoActionBar(context);
+                style = DialogStyle.NO_ACTION_BAR;
+                break;
+            case CURVE:
+                initializeCurve(context);
+                style = DialogStyle.CURVE;
+                break;
+            case FILL_STYLE:
+                initializeFill(context);
+                style = DialogStyle.FILL_STYLE;
+                break;
+            default:
+                initialize(context);
+                break;
+        }
+
     }
 
     protected CustomAlertDialog(@NonNull Context context, int themeResId) {
@@ -28,7 +77,7 @@ public class CustomAlertDialog extends AlertDialog {
         this.context = context;
         LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View view = li.inflate(R.layout.default_alert, null);
-        initialize(context,view);
+        initialize(context);
     }
 
     protected CustomAlertDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
@@ -36,37 +85,191 @@ public class CustomAlertDialog extends AlertDialog {
         this.context = context;
         LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View view = li.inflate(R.layout.default_alert, null);
-        initialize(context,view);
+        initialize(context);
     }
 
-    private void initialize(Context context,View alertView){
+    private void initialize(Context context){
+        LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = li.inflate(R.layout.default_alert, null);
+
+        positiveButton = view.findViewById(R.id.btn_positive);
+        negativeButton = view.findViewById(R.id.btn_negative);
+        alertMessage = view.findViewById(R.id.alert_message);
+        alertTitle = view.findViewById(R.id.alert_title);
+        llTitleLayout = view.findViewById(R.id.ll_title_layout);
+
         setCancelable(true);
-        setView(alertView);
+        setView(view);
+        setCanceledOnTouchOutside(true);
+    }
+
+    private void initializeNoActionBar(Context context){
+        LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = li.inflate(R.layout.no_actionbar_dialog, null);
+
+        positiveText = view.findViewById(R.id.btn_positive);
+        negativeText = view.findViewById(R.id.btn_negative);
+        alertMessage = view.findViewById(R.id.alert_message);
+        alertTitle = view.findViewById(R.id.alert_title);
+        dialogImage = view.findViewById(R.id.iv_dialog);
+
+        setCancelable(true);
+        setView(view);
+        setCanceledOnTouchOutside(true);
+    }
+
+    private void initializeCurve(Context context){
+        LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = li.inflate(R.layout.curve_layout, null);
+
+        positiveButton = view.findViewById(R.id.btn_positive);
+        negativeButton = view.findViewById(R.id.btn_negative);
+        alertMessage = view.findViewById(R.id.alert_message);
+        alertTitle = view.findViewById(R.id.alert_title);
+
+        setCancelable(true);
+        setView(view);
+        setCanceledOnTouchOutside(true);
+    }
+
+    private void initializeFill(Context context){
+        LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = li.inflate(R.layout.fill_dialog, null);
+
+        positiveButton = view.findViewById(R.id.btn_positive);
+        negativeButton = view.findViewById(R.id.btn_negative);
+        alertMessage = view.findViewById(R.id.alert_message);
+        dialogImage = view.findViewById(R.id.iv_dialog);
+        mainLayout = view.findViewById(R.id.main_layout);
+
+        setCancelable(true);
+        setView(view);
         setCanceledOnTouchOutside(true);
     }
 
     public void setAlertMessage(String message){
-        TextView textView = view.findViewById(R.id.alert_message);
-        textView.setText(message);
+        alertMessage.setText(message);
     }
 
     public void setPositiveButton(String name, View.OnClickListener onClickListener){
-        Button positiveButton = view.findViewById(R.id.btn_positive);
-        positiveButton.setText(name);
+        switch (style){
+            /*case DEFAULT:
+                positiveButton.setVisibility(View.VISIBLE);
+                positiveButton.setText(name);
+                positiveButton.setOnClickListener(onClickListener);
+                break;*/
+            case NO_ACTION_BAR:
+                positiveText.setVisibility(View.VISIBLE);
+                positiveText.setText(name);
+                positiveText.setOnClickListener(onClickListener);
+                break;
+                default:
+                    positiveButton.setVisibility(View.VISIBLE);
+                    positiveButton.setText(name);
+                    positiveButton.setOnClickListener(onClickListener);
+                    break;
 
-        positiveButton.setOnClickListener(onClickListener);
+        }
+
     }
 
     public void setNegativeButton(String name, View.OnClickListener onClickListener){
-        Button negativeButton = view.findViewById(R.id.btn_negative);
-        negativeButton.setText(name);
+        switch (style){
+            /*case DEFAULT:
+                negativeButton.setVisibility(View.VISIBLE);
+                negativeButton.setText(name);
+                negativeButton.setOnClickListener(onClickListener);
+                break;*/
+            case NO_ACTION_BAR:
+                negativeText.setVisibility(View.VISIBLE);
+                negativeText.setText(name);
+                negativeText.setOnClickListener(onClickListener);
+                break;
+                default:
+                    negativeButton.setVisibility(View.VISIBLE);
+                    negativeButton.setText(name);
+                    negativeButton.setOnClickListener(onClickListener);
+                    break;
 
-        negativeButton.setOnClickListener(onClickListener);
+        }
     }
 
     public void setAlertTitle(String title){
-        TextView textView = view.findViewById(R.id.alert_title);
-        textView.setText(title);
+        alertTitle.setText(title);
     }
 
+    public void setDialogType(DialogType type){
+        switch (type){
+            case INFO:
+                setColor(context.getResources().getColor(R.color.colorInfo),
+                        context.getResources().getColor(R.color.colorInfoTran),
+                        context.getResources().getDrawable(R.drawable.info_btn),
+                        context.getResources().getDrawable(R.drawable.cancel_info_btn),
+                        context.getResources().getDrawable(R.drawable.info_fill_btn),
+                        context.getResources().getDrawable(R.drawable.round_layout_info));
+                break;
+            case ERROR:
+                setColor(context.getResources().getColor(R.color.colorError),
+                        context.getResources().getColor(R.color.colorErrorTran),
+                        context.getResources().getDrawable(R.drawable.error_btn),
+                        context.getResources().getDrawable(R.drawable.cancel_error_btn),
+                        context.getResources().getDrawable(R.drawable.error_fill_btn),
+                        context.getResources().getDrawable(R.drawable.round_layout_error));
+                break;
+            case DEFAULT:
+                setColor(context.getResources().getColor(R.color.colorDefault),
+                        context.getResources().getColor(R.color.colorDefaultTran),
+                        context.getResources().getDrawable(R.drawable.default_btn),
+                        context.getResources().getDrawable(R.drawable.cancel_default_btn),
+                        context.getResources().getDrawable(R.drawable.default_fill_btn),
+                        context.getResources().getDrawable(R.drawable.round_layout_default));
+                break;
+            case SUCCESS:
+                setColor(context.getResources().getColor(R.color.colorSuccess),
+                        context.getResources().getColor(R.color.colorSuccessTran),
+                        context.getResources().getDrawable(R.drawable.success_btn),
+                        context.getResources().getDrawable(R.drawable.cancel_success_btn),
+                        context.getResources().getDrawable(R.drawable.success_fill_btn),
+                        context.getResources().getDrawable(R.drawable.round_layout_success));
+                break;
+            case WARNING:
+                setColor(context.getResources().getColor(R.color.colorWarning),
+                        context.getResources().getColor(R.color.colorWarningTran),
+                        context.getResources().getDrawable(R.drawable.warning_btn),
+                        context.getResources().getDrawable(R.drawable.cancel_warnign_btn),
+                        context.getResources().getDrawable(R.drawable.warning_fill_btn),
+                        context.getResources().getDrawable(R.drawable.round_layout_warning));
+                break;
+        }
+    }
+
+    private void setColor(int color,int colorTran, Drawable positiveDrawable, Drawable negativeDrawable, Drawable fillDrawable, Drawable layout){
+        switch (style){
+            case DEFAULT:
+                llTitleLayout.setBackgroundColor(color);
+                positiveButton.setBackground(positiveDrawable);
+                negativeButton.setBackground(negativeDrawable);
+                negativeButton.setTextColor(color);
+                break;
+            case NO_ACTION_BAR:
+                positiveText.setTextColor(color);
+                negativeText.setTextColor(color);
+                dialogImage.setColorFilter(color);
+                break;
+            case CURVE:
+                positiveButton.setBackground(fillDrawable);
+                break;
+            case FILL_STYLE:
+                mainLayout.setBackground(layout);
+
+
+        }
+    }
+
+    public void setDialogImage(Drawable image){
+        Glide.with(context)
+                .asDrawable()
+                .load(image)
+                .into(dialogImage);
+    }
 }
